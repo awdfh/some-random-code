7bf72e157f96b19e1cb32e25dca35721e17d285e
David Heebner <davidfh@aweber.com>
2016-01-14 11:10:40 -0500

    Testing some different code

--- kickit.py
+++ kickit.py
@@ -5,10 +5,10 @@ import random
 def lottery():
     # returns 6 numbers between 1 and 40
     for i in xrange(6):
-        yield random.randint(1, 40)
+        yield random.randint(1,1000)
 
     # returns a 7th number between 1 and 15
-    yield random.randint(1,15)
+    yield random.randint(1,50)
 
 for random_number in lottery():
     print "And the next number is... %d!" % random_number
