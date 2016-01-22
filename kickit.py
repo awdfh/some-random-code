ed98314cc74bc670642569a92ba2dcaf5ba512a4
David Heebner <davidfh@aweber.com>
2016-01-22 13:48:15 -0500

    Adding some more code to kickit.py

--- kickit.py
+++ kickit.py
@@ -10,5 +10,8 @@ def lottery():
     # returns a 7th number between 1 and 15
     yield random.randint(1,50)
 
+    for j in xrange(21):
+        yield random.randint(1,999999)
+
 for random_number in lottery():
     print "And the next number is... %d!" % random_number
